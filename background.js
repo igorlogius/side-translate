666c51afb71b17ac3a9d3d2e826c05e52989b5e5
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-09-12 14:19:23 +0200

    feat: add icon feat: add toolbar button fix: no translation on first try

--- background.js
+++ background.js
@@ -2,16 +2,48 @@
 
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
+const sidebarURL = browser.runtime.getURL("/sidebar.html");
+
+// feat: add tab link to translation history entry
+// feat: add delte link to history entry
+// fix/imp: reduce size of entries ???
+// feat: popout tranlsation into seperate window/popup for better reading?
+
+async function retrySend(text,rtcount){
+        if(rtcount < 1){
+            console.error('retry count went to zero', text);
+            return;
+        }
+        try {
+            await browser.runtime.sendMessage({text});
+        }catch(e){
+            console.error(e);
+            setTimeout( () => { retrySend(text, (rtcount-1)); }, 250);
+        }
+}
 
 browser.menus.create({
     title: extname,
     contexts: ["selection"],
     documentUrlPatterns: ["<all_urls>"],
     onclick: async (info /*,tab*/) => {
-        if(!(await browser.sidebarAction.isOpen({}))){
-            await browser.sidebarAction.open();
-        }
-        browser.runtime.sendMessage({text: info.selectionText.trim()})
+        setPanel();
+        browser.sidebarAction.open();
+        const text = info.selectionText.trim();
+        retrySend(text, 20);
     }
 });
+
+async function setPanel(){
+    const url= await browser.sidebarAction.getPanel({});
+    if(url !== sidebarURL){
+        await browser.sidebarAction.setPanel({panel: sidebarURL});
+    }
+}
+
+browser.browserAction.onClicked.addListener( async () => {
+    await browser.sidebarAction.open();
+    setPanel();
+});
+
 
